8f019009f3d8938bb57da033c904cc264527d24b
BENTOTV <agusjuniartha16@gmail.com>
2022-08-24 11:34:20 +0800

    menambahkan column dan text

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -5,8 +5,14 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Helo World"),
+    return  Center(
+      child:  Column(
+        children: const [
+          Text("Helo World"),
+          Text("Agus Juniartha")
+        ],
+      ),
+
     );
   }
 }
